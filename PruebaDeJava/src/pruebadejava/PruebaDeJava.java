335a353bbe71a122039c2473156476738b9338db
Adrián Arango <eladrian1608@gmail.com>
2019-10-30 11:05:29 -0500

    Está acometido:B

---
+++ PruebaDeJava/src/pruebadejava/PruebaDeJava.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebadejava;
+
+/**
+ *
+ * @author alumno
+ */
+public class PruebaDeJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
